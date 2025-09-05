c0884f6124ec9138793fbe22e4bde2f6e9f62a9a
TarasMykytiuk <tarasmykytiukw@gmail.com>
2025-09-06 00:45:07 +0300

    level 100

--- script.js
+++ script.js
@@ -6,7 +6,26 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for (const episode of episodeList) {
+    makeEpisodeCard(episode, rootElem);
+  }
+}
+
+function makeEpisodeCard(episode, rootElem) {
+  const episodeCode = "S" + String(episode.season) + "E" + String(episode.number).padStart(2, "0");
+  const cardDom = document.createElement("div");
+  cardDom.classList.add("card");
+  const nameDom = document.createElement("h2");
+  nameDom.textContent = episode.name + " - " + episodeCode;
+  const imageDom = document.createElement("img");
+  imageDom.src = episode.image.medium;
+  const summaryDom = document.createElement("p");
+  // original summary contained "<p>" at start and "</p>" at the end
+  summaryDom.textContent = episode.summary.substring(3, episode.summary.length - 4);
+  cardDom.appendChild(nameDom);
+  cardDom.appendChild(imageDom);
+  cardDom.appendChild(summaryDom);
+  rootElem.appendChild(cardDom);
 }
 
 window.onload = setup;
