69a672b544fb3c9b57abc5a8367359de92b817c7
TarasMykytiuk <tarasmykytiukw@gmail.com>
2025-10-01 22:18:07 +0300

    500

--- script.js
+++ script.js
@@ -33,26 +33,52 @@ async function getAllEpisodes(show_id) {
 async function fetchData() {
   const allShows = await getAllShows("https://api.tvmaze.com/shows");
   for (let i = 0; i < allShows.length; i++) {
-    fetchedShows[i + 1] = allShows[i];
-    const allEpisodes = await getAllEpisodes(i + 1);
-    fetchedEpisodes[i + 1] = allEpisodes;
+    fetchedShows[allShows[i].id] = allShows[i];
+    const allEpisodes = await getAllEpisodes(allShows[i].id);
+    fetchedEpisodes[allShows[i].id] = allEpisodes;
   }
 }
 
 async function setup() {
   await fetchData();
-  createShowsSelector(fetchedShows, "episodes");
+
+  //createShowsSelector(fetchedShows, "episodes");
+  renderShowsListingPage()
   loader.style.display = 'none';
 }
 
+function renderShowsListingPage() {
+  createShowInput();
+  createShowsSelector(fetchedShows, "shows");
+}
 
-function createShowsSelector(shows, pageListing) {
+function attachBackToShowListingButton() {
+  const backToShowListingButton = document.createElement("button");
+  backToShowListingButton.setAttribute("id", "backToShowListingButton");
+  backToShowListingButton.textContent = "Shows listing";
+  navBar.appendChild(backToShowListingButton);
+  backToShowListingButton.addEventListener("click", () => {
+    navBar.innerHTML = "";
+    renderShowsListingPage();
+  });
+}
+
+function createShowsSelector(shows, pageListing, selected_show_id = null) {
+  if (document.getElementById("show-selector-div") != null) {
+    document.getElementById("show-selector-div").remove();
+  }
+
+  if (document.getElementById("displayed-shows-quantity") != null) {
+    document.getElementById("displayed-shows-quantity").remove();
+  }
+
   const showSelector = document.createElement("select");
   showSelector.setAttribute("id", "show-selector");
   showSelector.setAttribute("name", "show-selector");
   showSelector.setAttribute("placeholder", "Chose show");
 
   const showSelectorDiv = document.createElement("div");
+  showSelectorDiv.setAttribute("id", "show-selector-div");
   showSelectorDiv.appendChild(showSelector);
   navBar.appendChild(showSelectorDiv);
 
@@ -65,11 +91,45 @@ function createShowsSelector(shows, pageListing) {
   }
 
   if (pageListing == "episodes") {
+    if (document.getElementById("show-input-div") != null) {
+      document.getElementById("show-input-div").remove();
+    }
+    if (document.getElementById("displayed-shows-quantity") != null) {
+      document.getElementById("displayed-shows-quantity").remove();
+    }
+    if (selected_show_id != null) {
+      showSelector.value = selected_show_id;
+    }
     showsEpisodesRender(showSelector.value);
+    attachBackToShowListingButton();
     showSelector.addEventListener("change", () => {
       const show_id = showSelector.value;
-      showsEpisodesRender(show_id)
+      showsEpisodesRender(show_id);
     });
+  }
+
+  if (pageListing == "shows") {
+    makePageForShows(shows);
+    showSelector.addEventListener("change", () => {
+      const show_id = showSelector.value;
+      createShowsSelector(shows, "episodes", show_id);
+    });
+    const showsQuantityDomDom = document.createElement("p");
+    showsQuantityDomDom.setAttribute("id", "displayed-shows-quantity")
+    showsQuantityDomDom.textContent = `Found ${Object.keys(shows).length} shows`;
+    navBar.insertBefore(showsQuantityDomDom, showSelectorDiv);
+  }
+}
+
+function makePageForShows(shows) {
+  rootElem.innerHTML = "";
+  // to apply different positioning of show or episodes cards
+  // new parent dom created
+  const showCardsDom = document.createElement("div");
+  showCardsDom.classList.add("show-cards-container");
+  rootElem.appendChild(showCardsDom);
+  for (const [id, show] of Object.entries(shows)) {
+    makeShowCard(show, showCardsDom);
   }
 }
 
@@ -158,6 +218,37 @@ function createEpisodesInput(allEpisodes) {
   });
 }
 
+function createShowInput() {
+  const beforeShowInputDom = document.createElement("p");
+  beforeShowInputDom.textContent = "Filtering for"
+  const showInputField = document.createElement("input");
+  showInputField.setAttribute("id", "show-input");
+  showInputField.setAttribute("placeholder", "Please insert your text");
+  showInputField.setAttribute("type", "search");
+
+  const showInputDiv = document.createElement("div");
+  showInputDiv.setAttribute("id", "show-input-div");
+  showInputDiv.appendChild(beforeShowInputDom);
+  showInputDiv.appendChild(showInputField);
+  navBar.appendChild(showInputDiv);
+
+  showInputField.addEventListener("input", () => {
+    let searchTerm = showInputField.value.toLowerCase();
+    // Update the page to be empty before filteredShows append
+    rootElem.innerHTML = "";
+    const searchMatch = {};
+    for (const [id, show] of Object.entries(fetchedShows)) {
+      if (show.name.toLowerCase().includes(searchTerm) ||
+        show.summary.toLowerCase().includes(searchTerm)) {
+        searchMatch[id] = show;
+      }
+    }
+    document.getElementById("displayed-shows-quantity").textContent = `Found ${searchMatch.length} shows`;
+    createShowsSelector(searchMatch, "shows");
+    makePageForShows(searchMatch);
+  });
+}
+
 function createCountArea(allEpisodes) {
   if (document.getElementById("count-area") != null) {
     document.getElementById("count-area").remove();
@@ -170,8 +261,11 @@ function createCountArea(allEpisodes) {
 
 function makePageForEpisodes(episodeList) {
   rootElem.innerHTML = "";
+  const episodesCardsDom = document.createElement("div");
+  episodesCardsDom.classList.add("episodes-cards-container");
+  rootElem.appendChild(episodesCardsDom);
   for (const episode of episodeList) {
-    makeEpisodeCard(episode);
+    makeEpisodeCard(episode, episodesCardsDom);
   }
 }
 
@@ -179,7 +273,7 @@ function getEpisodeCode(episode) {
   return "S" + String(episode.season) + "E" + String(episode.number).padStart(2, "0");
 }
 
-function makeEpisodeCard(episode) {
+function makeEpisodeCard(episode, parentDom) {
   const episodeCode = getEpisodeCode(episode);
   const cardDom = document.createElement("div");
   cardDom.classList.add("card");
@@ -193,7 +287,88 @@ function makeEpisodeCard(episode) {
   cardDom.appendChild(nameDom);
   cardDom.appendChild(imageDom);
   cardDom.appendChild(summaryDom);
-  rootElem.appendChild(cardDom);
+  parentDom.appendChild(cardDom);
+}
+
+function makeShowCard(show, parentDom) {
+  const cardDom = document.createElement("div");
+  cardDom.classList.add("show-card");
+
+  const nameDom = document.createElement("h1");
+  nameDom.textContent = show.name;
+  // if show name clicked, episodes listing displayed
+  nameDom.addEventListener("click", () => {
+    createShowsSelector(fetchedShows, "episodes", show.id);
+  });
+
+  const contentDom = document.createElement("div");
+  contentDom.classList.add("show-content");
+  const imageDom = document.createElement("img");
+  imageDom.src = show.image.medium;
+  const summaryDom = document.createElement("p");
+  // original summary contained "<p>" at start and "</p>" at the end
+  summaryDom.innerHTML = show.summary.substring(3, show.summary.length - 4);
+
+  const infoDom = document.createElement("div");
+  infoDom.classList.add("show-info");
+  const ratingDom = document.createElement("div");
+  ratingDom.classList.add("show-info-item");
+  const ratingNameDom = document.createElement("p");
+  ratingNameDom.classList.add("info-item-name");
+  ratingNameDom.textContent = "Rated:"
+  const ratingValueDom = document.createElement("p");
+  ratingValueDom.textContent = show.rating.average;
+  ratingDom.appendChild(ratingNameDom);
+  ratingDom.appendChild(ratingValueDom);
+
+  const genresDom = document.createElement("div");
+  genresDom.classList.add("show-info-item");
+  const genresNameDom = document.createElement("p");
+  genresNameDom.classList.add("info-item-name");
+  genresNameDom.textContent = "Genres:"
+  const genresValueDom = document.createElement("p");
+  // add all genres to one string
+  let genresStr = "";
+  const genres = show.genres;
+  for (let i = 0; i < genres.length; i++) {
+    i != genres.length - 1 ? genresStr += genres[i] + " | " : genresStr += genres[i];
+  }
+  genresValueDom.textContent = genresStr;
+  genresDom.appendChild(genresNameDom);
+  genresDom.appendChild(genresValueDom);
+
+  const statusDom = document.createElement("div");
+  statusDom.classList.add("show-info-item");
+  const statusNameDom = document.createElement("p");
+  statusNameDom.classList.add("info-item-name");
+  statusNameDom.textContent = "Status:"
+  const statusValueDom = document.createElement("p");
+  statusValueDom.textContent = show.status;
+  statusDom.appendChild(statusNameDom);
+  statusDom.appendChild(statusValueDom);
+
+  const runtimeDom = document.createElement("div");
+  runtimeDom.classList.add("show-info-item");
+  const runtimeNameDom = document.createElement("p");
+  runtimeNameDom.classList.add("info-item-name");
+  runtimeNameDom.textContent = "Runtime:"
+  const runtimeValueDom = document.createElement("p");
+  runtimeValueDom.textContent = show.runtime;
+  runtimeDom.appendChild(runtimeNameDom);
+  runtimeDom.appendChild(runtimeValueDom);
+
+  infoDom.appendChild(ratingDom);
+  infoDom.appendChild(genresDom);
+  infoDom.appendChild(statusDom);
+  infoDom.appendChild(runtimeDom);
+
+  cardDom.appendChild(nameDom);
+  contentDom.appendChild(imageDom);
+  contentDom.appendChild(summaryDom);
+  contentDom.appendChild(infoDom);
+
+  cardDom.appendChild(contentDom);
+  parentDom.appendChild(cardDom);
 }
 
 function countEpisodes(countAllEpisodes, countFilteredEpisodes) {
